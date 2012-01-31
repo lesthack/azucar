0fddd586dcbfa764abbfaa9fea32a649e636eb8a
Jorge Luis Hernández C <lesthack@gmail.com>
2012-01-30 18:33:06 -0600

    Update

--- src/player.py
+++ src/player.py
@@ -648,27 +648,27 @@ class player:
 
     
     
-class album:    
-    def __init__(self, album, artist, url_cover):
-        self.album = album
-        self.artist = artist
-        self.url_cover = url_cover
+# class album:    
+    # def __init__(self, album, artist, url_cover):
+        # self.album = album
+        # self.artist = artist
+        # self.url_cover = url_cover
         
-class albums:
-    def __init__(self):
-        self.list = []
-        self._list = []
-        pass
+# class albums:
+    # def __init__(self):
+        # self.list = []
+        # self._list = []
+        # pass
     
-    def add_album(self, album, artist, url_cover):
-        if album not in self.list:
-            self.list.append(album)
-            self._list.append( album(album, artist, url_cover) )
+    # def add_album(self, album, artist, url_cover):
+        # if album not in self.list:
+            # self.list.append(album)
+            # self._list.append( album(album, artist, url_cover) )
     
     
-    def get_album(self, album):
-        if album in self.list:
-            return self._list[]
+    # def get_album(self, album):
+        # if album in self.list:
+            # return self._list[]
     
     
     
